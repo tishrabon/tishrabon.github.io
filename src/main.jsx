3af5ab0ed30bf84e0fb93b1900cb7f2711129c75
tishrabon-code <tishrabon.code@gmail.com>
2025-02-20 14:41:16 +0600

    replace BrowserRouter with HashROuter

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { ThemeProvider } from "./context/ThemeContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter >
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter >
   </StrictMode>,
 );
